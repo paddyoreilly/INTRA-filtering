447e711573d0f4a8266dc50f02eca903bf93a4f1
paddyoreilly <80277287+paddyoreilly@users.noreply.github.com>
2021-03-23 20:17:28 +0000

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -199,7 +199,7 @@ def interpolateprocess(datacleaned):
     return datainterp
 
 
-def clean_and_interp(data_F,c1=20,c2=2,c3=20,c4=2,int_lim=0.5):
+def clean_I(data_F,c1=20,c2=2,c3=20,c4=2,int_lim=0.5):
     """
     Removes the datapoints which are outside of the limits set by the constants and interpolates it into a full image.
     Returns a full Numpy array.
@@ -213,7 +213,7 @@ def clean_and_interp(data_F,c1=20,c2=2,c3=20,c4=2,int_lim=0.5):
     c2 : The limit as a multiple of standard dev of derivative, optional
         The default is 2.
     c3 : The absolute limit of 2nd derivative, optional
-        DESCRIPTION. The default 20.
+         The default 20.
     c4 : The limit as a multiple of standard dev of 2nd derivative, optional
         The default is 2.
     int_lim : The upper limit of the intensity, optional
@@ -228,7 +228,7 @@ def clean_and_interp(data_F,c1=20,c2=2,c3=20,c4=2,int_lim=0.5):
     
     return interpolateprocess(cleaningprocess(data_F,c1,c2,c3,c4,int_lim))
 
-def pcolormeshplot(data, y, x, vdata=None, tfs=False, title="", colorbar=False,cmap=None,alpha=None):
+def pcolormeshplot(data, y, x, vdata=None, tfs=False, title="", colorbar=False,cmap=None,alpha=None,figsize=None):
     """
     Uses pyplot's pcolormesh function to plot data
 
@@ -242,16 +242,19 @@ def pcolormeshplot(data, y, x, vdata=None, tfs=False, title="", colorbar=False,c
         The x values that will be plotted (t_arr).
     vdata : Numpy array
         Used for vmax and vmin.
-    tfs : True or False, optional
+    tfs : Bool, optional
         If False, just plots 10-90 MHz. The default is False.
     title : String, optional
         Title of the plot. The default is "".
-    colorbar : True or False, optional
+    colorbar : Bool, optional
         Adds a colorbar. The default is False.
     cmap : String, optional
         Change the colormap of the plot. The default is None.
     alpha : Float, optional
         Change the opacity of the plot. The default is None.
+    figsize : Tuple, optional
+        Sets the size of the plot in inches (400dpi). The default is None.
+
 
     Returns
     -------
@@ -263,6 +266,11 @@ def pcolormeshplot(data, y, x, vdata=None, tfs=False, title="", colorbar=False,c
     g1 = 200
     g2 = 400
         
+    if figsize is None:
+        pass
+    else:
+        fig, ax = plt.subplots(figsize=figsize, dpi=400)
+    
     plt.pcolormesh(x,y[:g1],data.T[:g1], shading='auto',
                    vmin=np.percentile(vdata,2), vmax=np.percentile(vdata,98),cmap=cmap,alpha=alpha);
     if tfs == True:
@@ -283,3 +291,158 @@ def pcolormeshplot(data, y, x, vdata=None, tfs=False, title="", colorbar=False,c
         
     plt.title(title)
     #plt.show()
+    
+    
+def badchannelfinder(data,cut=0.75,upper_lim=20,lower_lim=0.5, histreturn=False, plot=False):
+    """
+    Seeks out bad channels.
+    First defines a 'normal' limit. Any channel outside this limit for longer than the cutoff time is placed inside a list.
+
+    Parameters
+    ----------
+    data : Numpy array
+        The data array.
+    cut : The cutoff point as a multiple of the time for bad bands, optional
+        ie. If a band is outside the limit for 0.75 of the time it is considered bad. The default is 0.75.
+    upper_lim : Upper limit for bad bands, optional
+        The default is 20.
+    lower_lim : Lower limit for bad bands, optional
+        The default is 0.5.
+    plot : Plot the data array alongside a histogram with counts outside the limit, optional
+        The default is False.
+    histreturn : Return the histogram of counts outside the limit as hist, optional
+        The default is False.
+
+    Returns
+    -------
+    bad : List
+        List of bands outside the limit for longer than the cutoff.
+
+    """
+    
+    if histreturn == True:
+        global hist
+    hist = []
+    for i in range(len(data.T)):
+        count = 0
+        for s in range(len(data)):
+            if data[s,i] >= upper_lim or data[s,i] <= lower_lim:
+                count = count+1
+        hist.append(count)
+    
+    bad = []
+    for i in range(len(hist)):
+        if hist[i] >= cut*len(data):
+            bad.append(i)
+    
+    if plot == True:
+        fig, ax = plt.subplots(figsize=(15,10), dpi=400)
+        
+        plt.subplot2grid(shape=(1,4),loc=(0,0),colspan=3)
+        pcolormeshplot(data,freqs,t_arr,tfs=True,title="Bad channels are outside limit for "+str(round(cut*100))+"% of the whole observation")
+        
+        for i in bad:
+            plt.axhline(freqs[i],c='r',alpha=0.5)
+        
+        plt.subplot2grid(shape=(1,4),loc=(0,3))
+        plt.barh(np.arange(len(freqs)),hist,1)
+        
+        plt.ylim(0,len(freqs))
+        plt.xlim(0,st.stdev(hist)*5+np.average(hist))
+        
+        plt.gca().yaxis.set_major_locator(MultipleLocator(20))
+        plt.gca().yaxis.set_minor_locator(MultipleLocator(2))
+        
+        plt.ylabel("Subbands")
+        plt.xlabel("Counts")
+        
+        plt.title("Counts of Values over "+str(upper_lim)+" or under "+str(lower_lim))
+        
+        plt.axvline(cut*len(data),c="r")
+        
+        plt.tight_layout()
+        plt.show()
+    
+    return bad
+
+
+def badchannelremover(data,cut=0.75,upper_lim=20,lower_lim=0.5,bad=None):
+    """
+    Seeks out bad channels and removes them.
+    If a list of bad channels is given, it will remove them instead of seeking the bad channels out.
+
+    Parameters
+    ----------
+    data : Numpy array
+        The data array.
+    cut : The cutoff point as a multiple of the time for bad bands, optional
+        ie. If a band is outside the limit for 0.75 of the time it is considered bad. The default is 0.75.
+    upper_lim : Upper limit for bad bands, optional
+        The default is 20.
+    lower_lim : Lower limit for bad bands, optional
+        The default is 0.5.
+    bad : If you want to define the bad bands yourself, optional
+        The default is None.
+
+    Returns
+    -------
+    data : Numpy array
+        Array with bad bands removed.
+
+    """
+    data_n = data.copy()
+    
+    if bad is None:
+        bad = badchannelfinder(data,cut=len(t_arr)*0.75,upper_lim=20,lower_lim=0.5)
+                
+    for i in bad:
+        data_n.T[i] = np.NaN*len(freqs)
+        
+    return data_n
+
+
+def completeclean(data,cut=0.75,upper_lim=20,lower_lim=0.5,c1=20,c2=2,c3=20,c4=2,int_lim=0.5, bad=None, interpolate=True):
+    """
+    Runs the complete cleaning process, including removal of bad subbands.
+
+    Parameters
+    ----------
+    data : Numpy array
+        The uncleaned data array.
+    cut : The cutoff point as a multiple of the time for bad bands, optional
+        ie. If a band is outside the limit for 0.75 of the time it is considered bad. The default is 0.75.
+    upper_lim : Upper limit for bad bands, optional
+        The default is 20.
+    lower_lim : Lower limit for bad bands, optional
+        The default is 0.5.
+    c1 : The absolute limit of derivative, optional
+        The default is 20.
+    c2 : The limit as a multiple of standard dev of derivative, optional
+        The default is 2.
+    c3 : The absolute limit of 2nd derivative, optional
+         The default 20.
+    c4 : The limit as a multiple of standard dev of 2nd derivative, optional
+        The default is 2.
+    int_lim : The upper limit of the intensity, optional
+        DESCRIPTION. The default is 0.5.
+    bad : If you want to define the bad bands yourself, optional
+        The default is None.
+    interpolate : If you want the end result interpolated, optional
+        The default is True.
+
+    Returns
+    -------
+    Numpy array
+        Clean array with bad bands removed.
+
+    """
+    if bad is None:
+        bad = badchannelfinder(data,cut,upper_lim,lower_lim) 
+    data_n = badchannelremover(cleaningprocess(data,c1,c2,c3,c4,int_lim),bad=bad)
+    
+    if interpolate == True:
+        return interpolateprocess(data_n)
+    else:
+        return data_n
+
+
